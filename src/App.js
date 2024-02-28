b09d0c4fa8d1eb1bb3834ac33ef0099dc953aa83
archiKumari <archikumari97@gmail.com>
2024-02-28 20:25:03 +0530

    Designed basic homepage structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
 
 function App() {
 
   return (
     <Container> 
-      <p className="heading-text">HI, MY NAME IS ARCHI.</p>
-      <p className="heading-subtext">I AM A WEB DEVELOPER</p>
+      <p className="heading1">HI, I AM ARCHI.</p>
+      <p className="heading2">SOFTWARE, WEB AND MOBILE DEVELOPER</p>
+      <div style={{padding:200}}></div>
+      <Divider textAlign="left"><p className="heading3">EXPERTISE</p></Divider>
+      <div style={{padding:200}}></div>
+      <Divider textAlign="left"><p className="heading3">WORK</p></Divider>
+      <div style={{padding:200}}></div>
+      <Divider textAlign="left"><p className="heading3">EXPERIENCE</p></Divider>
+      <div style={{padding:200}}></div>
     </Container>
   );
 
