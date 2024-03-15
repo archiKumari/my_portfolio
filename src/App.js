6f67f44d86ddb73dc60e5daf8d0db61ff5895aaf
archiKumari <archikumari97@gmail.com>
2024-03-15 22:02:54 +0530

    Added top bar for displaying current section name

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
 import "./App.css";
-import Separator from "./Components/Separator";
-import MyDivider from "./Components/Divider";
-
+import { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 
+import Separator from "./Components/Separator";
+
 function App() {
+  const [currentSection, setCurrentSection] = useState("ABCD");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll("section");
+      const scrollPosition = window.scrollY;
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionBottom = sectionTop + section.offsetHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+          setCurrentSection(section.getAttribute("id"));
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [currentSection]);
+
   return (
-    <>
-      <Container>
+    <div>
+      <div className="top-bar">
+        <p style={{color:"black", fontWeight:"bold"}}>{currentSection}</p>
+      </div>
+      <section id="ABOUT">
         <p className="heading1">HI, I AM ARCHI.</p>
         {/* <p className="heading2">SOFTWARE, WEB AND MOBILE DEVELOPER</p> */}
         <p className="heading2">WEB DEVELOPER</p>
-        <Separator />
-        <p className="heading3">SKILLS</p>
-        <p className="heading4">Frontend: React, React Native, HTML, CSS, Javascript</p>
+      </section>
+      <section id="SKILLS">
+        {/* <p className="heading3">SKILLS</p> */}
+        <p className="heading4">
+          Frontend: React, React Native, HTML, CSS, Javascript
+        </p>
         <p className="heading4">Backend: Haskell, NodeJS, Postgres</p>
-        <p className="heading4">Tools: Git, Figma, Webflow, OS - Linux and Windows</p>
-        <Separator />
-        <p className="heading3">WORK</p>
+        <p className="heading4">
+          Tools: Git, Figma, Webflow, OS - Linux and Windows
+        </p>
+      </section>
+      <section id="WORK">
+        {/* <p className="heading3">WORK</p> */}
         <p className="heading4">Phonebook: Web Application</p>
         <p className="heading4">Expense Tracker: Mobile Application</p>
         <p className="heading4">Snake Game: Haskell Application</p>
-        <Separator />
-        <p className="heading3">EXPERIENCE</p>
-        <p className="heading4">Frontend Developer (Remote), Maa Gyan Services</p>
-        <p className="heading4">Mobile Application Developer (Remote), Petdrifts</p>
-        <p className="heading4">Frontend Developer (Remote), Betiyan Nidhi Limited</p>
-        <p></p>
-        <Separator />
-      </Container>
-    </>
+      </section>
+      <section id="EXPERIENCE">
+        {/* <p className="heading3">EXPERIENCE</p> */}
+        <p className="heading4">
+          Frontend Developer (Remote), Maa Gyan Services
+        </p>
+        <p className="heading4">
+          Mobile Application Developer (Remote), Petdrifts
+        </p>
+        <p className="heading4">
+          Frontend Developer (Remote), Betiyan Nidhi Limited
+        </p>
+      </section>
+    </div>
   );
 }
 
