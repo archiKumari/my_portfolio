1fe7640b1a8b0e7900bddc89f0bafc2c11104db6
archiKumari <archikumari97@gmail.com>
2024-03-18 20:08:45 +0530

    Added divider components

---
+++ src/Components/Dividers/HorizontalDivider.jsx
@@ -0,0 +1,5 @@
+const HorizontalDivider = () => {
+    return <div style={{ width:'95%', border: '1px solid rgba(250, 250, 250, 0.5)', borderRadius:'5px' }}></div>
+}
+
+export default HorizontalDivider;
