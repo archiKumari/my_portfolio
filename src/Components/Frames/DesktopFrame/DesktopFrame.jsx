5a4733faf02c375bd88013a6e744ee099a656e38
archiKumari <archikumari97@gmail.com>
2024-04-04 11:23:03 +0530

    Modified frame components

--- src/Components/Frames/DesktopFrame/DesktopFrame.jsx
+++ src/Components/Frames/DesktopFrame/DesktopFrame.jsx
@@ -1,12 +1,26 @@
 import "./DesktopFrame.css";
 
-import projectImg1 from "../../../Assets/Work-images/BNL/image1.png";
+import projectImg1 from "../../../Assets/Work-images/BNL/image3.png";
+import Badge from "../../Badge/Badge";
 
 const DesktopFrame = () => {
+  const skills = ["React", "NodeJs", "MongoDB", "Photoshop"];
   return (
-    <div>
-      <div className="desktop-container">
-        <img className="desktop-image" src={projectImg1} />
+    <div className="desktop-container">
+      <p className="desktop-title">Betiyan Nidhi Limited</p>
+      <div className="desktop-content">
+        <img className="desktop-image" alt="Website Homepage" src={projectImg1} />
+      </div>
+      <div className="desktop-text">
+        <div>
+          <p className="text-header">Frontend Developer</p>
+          <p className="subtext">Aug 2022 - Dec 2022</p>
+        </div>
+        <div className="badge-container">
+          {skills.map((item) => (
+            <Badge text={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/Components/Frames/MobileFrame/MobileFrame.jsx
+++ src/Components/Frames/MobileFrame/MobileFrame.jsx
@@ -1,11 +1,27 @@
 import "./MobileFrame.css";
 import projectImg1 from "../../../Assets/Project-images/Petdrifts/Homepage.png";
+import Badge from "../../Badge/Badge";
 
 const MobileFrame = () => {
+  const skills = ["React Native", "Figma"];
   return (
-      <div className="mobile-container">
-        <img className="mobile-image" src={projectImg1} />
+    <div className="mobile-container">
+      <p className="mobile-title">Petdrifts Mobile App</p>
+      <div className="mobile-content">
+        <img className="mobile-image" alt="Mobile App" src={projectImg1} />
       </div>
+      <div className="mobile-text">
+        <div>
+          <p className="text-header">Mobile App Developer</p>
+          <p className="subtext">Jan 2023 - Apr 2023</p>
+        </div>
+        <div className="badge-container">
+          {skills.map((item) => (
+            <Badge text={item} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
