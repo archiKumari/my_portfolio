70955d44ca9914a544aa6d7bfbc4c79209be8516
archiKumari <archikumari97@gmail.com>
2024-04-09 21:50:53 +0530

    Hover animation added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import "./App.css";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+import { MouseEvent } from "react";
 
+import "./App.css";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import SkillsSection from "./Components/SkillsSection/SkillsSection";
 import WorkSection from "./Components/WorkSection/WorkSection";
@@ -8,8 +10,28 @@ import Contact from "./Components/ContactSection/Contact";
 import HorizontalDivider from "./Components/Dividers/HorizontalDivider";
 
 function App() {
+  let mouseX = useMotionValue(0);
+  let mouseY = useMotionValue(0);
+
+  function handleMouseMove({clientX,clientY,currentTarget}) {
+    console.log("render");
+    const {left,top} = currentTarget.getBoundingClientRect();
+    let xPosition = clientX - left;
+    let yPosition = clientY - top;
+
+    mouseX.set(xPosition);
+    mouseY.set(yPosition);
+
+  }
+  // rgba(3,12,26,1)
   return (
-    <div className="main-container">
+    <motion.div
+      className="main-container"
+      onMouseMove={handleMouseMove}
+      style={{
+        background: useMotionTemplate`radial-gradient(300px circle at ${mouseX}px ${mouseY}px ,rgba(15,27,77,1),rgba(1,16,41,1))`,
+      }}
+    >
       <section id="ABOUT">
         <HeroSection />
       </section>
@@ -33,7 +55,7 @@ function App() {
         <p className="heading3">CONTACT</p>
         <Contact />
       </section>
-    </div>
+    </motion.div>
   );
 }
 
