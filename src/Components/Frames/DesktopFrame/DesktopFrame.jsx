a12a4d375d7401957ca0e360cdb372796924be57
archiKumari <archikumari97@gmail.com>
2024-04-11 15:15:46 +0530

    Scroll Animations added in work section

--- src/Components/Frames/DesktopFrame/DesktopFrame.jsx
+++ src/Components/Frames/DesktopFrame/DesktopFrame.jsx
@@ -1,28 +1,30 @@
-import { useState } from "react";
+import { motion } from "framer-motion";
 
 import "./DesktopFrame.css";
 import Badge from "../../Badge/Badge";
 
-const DesktopFrame = ({ title,heading,subtext,skills,imagePath }) => {
-
+const DesktopFrame = ({ title, heading, subtext, skills, imagePath }) => {
   return (
-    <div className="desktop-container">
-      <p className="desktop-title">{title}</p>
-      <div className="desktop-content">
+    <motion.div
+      className="desktop-container"
+      initial={{opacity:0 }}
+      transition={{ duration: 1,delay:0.3 }}
+      whileInView={{ opacity:1}}
+    >
+      <div className="desktop-image-container">
         <img className="desktop-image" alt="Website Homepage" src={imagePath} />
       </div>
-      <div className="desktop-text">
-        <div>
-          <p className="text-header">{heading}</p>
-          <p className="subtext">{subtext}</p>
-        </div>
+      <div className="desktop-text-container">
+        <p className="desktop-title">{title}</p>
+        <p className="text-header">{heading}</p>
+        <p className="subtext">{subtext}</p>
         <div className="badge-container">
           {skills.map((item) => (
             <Badge text={item} />
           ))}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/Components/Frames/MobileFrame/MobileFrame.jsx
+++ src/Components/Frames/MobileFrame/MobileFrame.jsx
@@ -1,26 +1,30 @@
+import { motion } from "framer-motion";
+
 import "./MobileFrame.css";
 import Badge from "../../Badge/Badge";
 
-const MobileFrame = ({ title,heading,subtext,skills,imagePath }) => {
-  
+const MobileFrame = ({ title, heading, subtext, skills, imagePath }) => {
   return (
-    <div className="mobile-container">
-      <p className="mobile-title">{title + " App"}</p>
-      <div className="mobile-content">
+    <motion.div
+      className="mobile-container"
+      initial={{ opacity: 0 }}
+      transition={{ duration: 1.5, delay: 0.2 }}
+      whileInView={{ opacity: 1 }}
+    >
+      <div className="mobile-image-container">
         <img className="mobile-image" alt="Mobile App" src={imagePath} />
       </div>
-      <div className="mobile-text">
-        <div>
-          <p className="text-header">{heading}</p>
-          <p className="subtext">{subtext}</p>
-        </div>
+      <div className="mobile-text-container">
+        <p className="mobile-title">{title + " App"}</p>
+        <p className="text-header">{heading}</p>
+        <p className="subtext">{subtext}</p>
         <div className="badge-container">
           {skills.map((item) => (
             <Badge text={item} />
           ))}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
