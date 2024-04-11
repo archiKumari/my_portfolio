73e0468e2cc41d37d94d7f80a9dbb6e0f97271f0
archiKumari <archikumari97@gmail.com>
2024-04-11 11:23:04 +0530

    Improved the hover animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
-import { MouseEvent } from "react";
 
 import "./App.css";
 import HeroSection from "./Components/HeroSection/HeroSection";
@@ -10,26 +9,24 @@ import Contact from "./Components/ContactSection/Contact";
 import HorizontalDivider from "./Components/Dividers/HorizontalDivider";
 
 function App() {
-  let mouseX = useMotionValue(0);
-  let mouseY = useMotionValue(0);
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
 
   function handleMouseMove({clientX,clientY,currentTarget}) {
-    console.log("render");
     const {left,top} = currentTarget.getBoundingClientRect();
     let xPosition = clientX - left;
     let yPosition = clientY - top;
 
     mouseX.set(xPosition);
     mouseY.set(yPosition);
-
   }
-  // rgba(3,12,26,1)
+
   return (
     <motion.div
       className="main-container"
       onMouseMove={handleMouseMove}
       style={{
-        background: useMotionTemplate`radial-gradient(300px circle at ${mouseX}px ${mouseY}px ,rgba(15,27,77,1),rgba(1,16,41,1))`,
+        background: useMotionTemplate`radial-gradient(250px circle at ${mouseX}px ${mouseY}px ,rgba(14,41,150,2),rgba(2,16,71,1))`,
       }}
     >
       <section id="ABOUT">
