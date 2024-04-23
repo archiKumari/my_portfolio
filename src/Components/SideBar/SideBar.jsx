dfdce915c18fc88ce69bd7540db56f6a4ba6ec5c
archiKumari <archikumari97@gmail.com>
2024-04-23 11:37:09 +0530

    Sidebar added

---
+++ src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import "./SideBar.css";
+import Links from "./Links/Links";
+import ToggleButton from "./ToggleButton/ToggleButton";
+
+const SideBar = () => {
+  const [isOpen, setOpen] = useState(false);
+  const variants = {
+    opened: {
+      clipPath: "circle(1200px at 50px 50px)",
+      transition: {
+        type: "spring",
+        stiffness: 20,
+      },
+    },
+    closed: {
+      clipPath: "circle(30px at 50px 50px)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
+  return (
+    <motion.div className="sidebar" animate={isOpen ? "opened" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
+    </motion.div>
+  );
+};
+export default SideBar;
