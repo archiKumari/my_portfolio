35b0f14abf3dda0db1d06a95c9a122598c63be01
archiKumari <archikumari97@gmail.com>
2024-04-23 15:06:21 +0530

    Hero section text and animations added

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,11 +1,77 @@
+import { motion } from "framer-motion";
+
 import "./HeroSection.css";
+import scrollImg from "../../Assets/scroll.png";
 
 const HeroSection = () => {
+  const textVariants = {
+    initial: {
+      x: -500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.2,
+      },
+    },
+    scrollButton: {
+      opacity: 0,
+      y: 10,
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+      },
+    },
+  };
+
+  const sliderVariants = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: "-300%",
+      opacity: 1,
+      transition: {
+        duration: 20,
+        repeat: Infinity,
+        repeatType: "mirror"
+      },
+    },
+  };
+
   return (
-    <div>
-      <p className="heading1">HI, I AM ARCHI.</p>
-      {/* <p className="heading2">SOFTWARE, WEB AND MOBILE DEVELOPER</p> */}
-      <p className="heading2">WEB DEVELOPER</p>
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="text-container"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>ARCHI KUMARI</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Web, Mobile and Haskell Developer{" "}
+          </motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              See Latest Works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            src={scrollImg}
+            alt="scroll indicator"
+            variants={textVariants}
+            animate="scrollButton"
+          />
+        </motion.div>
+      </div>
+      <motion.div className="sliding-text-container" variants={sliderVariants} initial="initial" animate="animate">
+        Web Mobile Haskell UI UX
+      </motion.div>
     </div>
   );
 };
