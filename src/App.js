b3298446f8635b14e9b8e7d09940649acd9d58d9
archiKumari <archikumari97@gmail.com>
2024-05-19 14:26:05 +0530

    Custom cursor added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import { motion } from "framer-motion";
-
 import "./App.css";
-import SideBar from "./Components/SideBar/SideBar"
+import Cursor from "./Components/Cursor/Cursor";
+import SideBar from "./Components/SideBar/SideBar";
 import NavBar from "./Components/NavBar/NavBar";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import AboutSection from "./Components/AboutSection/AboutSection";
-import SkillsSection from "./Components/SkillsSection/SkillsSection"
+import SkillsSection from "./Components/SkillsSection/SkillsSection";
 import PortfolioSection from "./Components/PortfolioSection/PortfolioSection";
 import Contact from "./Components/ContactSection/Contact";
 
 function App() {
   return (
-    <motion.div className="main-container">
-      <SideBar/>
-      <NavBar />
+    <div className="main-container">
+      <Cursor />
       <section id="/">
+        <SideBar />
+        <NavBar />
         <HeroSection />
       </section>
       <section id="About">
         <AboutSection />
       </section>
       <section id="Skills">
-        <SkillsSection/>
+        <SkillsSection />
       </section>
       <PortfolioSection />
       <section id="Contact">
         <Contact />
       </section>
-    </motion.div>
+    </div>
   );
 }
 
